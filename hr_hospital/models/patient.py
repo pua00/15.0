01928a757673ed32d802ae1cd2e775b5df747f1d
Ruslan Panchenko <pua@ukr.net>
2022-10-28 16:53:20 +0300

    [IMP] patient: add computed field "age"

--- hr_hospital/models/patient.py
+++ hr_hospital/models/patient.py
@@ -1,6 +1,8 @@
 import logging
 
-from odoo import fields, models
+from odoo import api, fields, models
+from datetime import date
+
 
 _logger = logging.getLogger(__name__)
 
@@ -12,6 +14,15 @@ class Patient(models.Model):
     name = fields.Char(string='Full name', required=True)
     gender = fields.Selection(string='Gender', selection=[('male', "Male"), ('femail', "Femail")], default='male', required=True)
     date_of_birth = fields.Date(string='Date of birth')
-    age = fields.Integer(string='Age of patient')
+    age = fields.Integer(string='Age of patient', compute='_computed_age')
     passport_date = fields.Char(string='Passport date')
     contact_person = fields.Char(string='Person for contact')
+
+    @api.depends('date_of_birth')
+    def _computed_age(self):
+        today = date.today()
+        for rec in self:
+            if rec.date_of_birth:
+                rec.age = today.year - rec.date_of_birth.year
+            else:
+                rec.age = 1
