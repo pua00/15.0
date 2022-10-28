5f4c59301cc05f1bb2409ce3e8bcfcc353391572
Ruslan Panchenko <pua@ukr.net>
2022-10-28 17:47:33 +0300

    [IMP] doctor: add abstract class Person

--- hr_hospital/models/doctor.py
+++ hr_hospital/models/doctor.py
@@ -1,6 +1,7 @@
 import logging
 
 from odoo import fields, models
+from . import person
 
 _logger = logging.getLogger(__name__)
 
@@ -8,6 +9,7 @@ _logger = logging.getLogger(__name__)
 class Doctor(models.Model):
     _name = 'hr_hospital.doctor'
     _description = 'Doctor'
+    _inherit = {'hr_hospital.person': 'person_id'}
 
-    name = fields.Char(string='Full name', required=True)
+    person_id = fields.Many2one(comodel_name='hr_hospital.person')
     profession = fields.Char(string='Profession', required=True)
