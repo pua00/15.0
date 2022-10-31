ef1409bee08c97fb573e5823a7826b167b9bce23
Ruslan Panchenko <pua@ukr.net>
2022-10-31 17:48:55 +0200

    ''[IMP] schedlue_of_doctor: add onchange (on datetime)'

--- hr_hospital/models/schedule_of_doctor.py
+++ hr_hospital/models/schedule_of_doctor.py
@@ -1,6 +1,6 @@
 import logging
 
-from odoo import fields, models
+from odoo import api, fields, models
 
 _logger = logging.getLogger(__name__)
 
@@ -10,15 +10,21 @@ class ScheduleOfDoctor(models.Model):
     _description = 'Schedule of Doctor'
 
     name = fields.Char(string='Schedule of Doctor', compute='_computed_name')
-    doctor_id = fields.Many2one(comodel_name='hr_hospital.doctor', string='Doctor', required=True, index=True)
-    visit_date = fields.Date(string='Date for visit', required=True)
-    visit_from_date = fields.Datetime(string='Visit from', required=True)
-    visit_to_date = fields.Datetime(string='Visit to', required=True)
+    doctor_id = fields.Many2one(comodel_name='hr_hospital.doctor', string='Doctor',
+                                required=True, index=True)
+    visit_date = fields.Date(string='Date', required=True)
+    visit_start_datetime = fields.Datetime(string='Visit start from', required=True)
     active = fields.Boolean(default=True)
 
     def _computed_name(self):
         for rec in self:
-            rec.name = 'Doc schedule'
-            # rec.name = rec.doctor_id
-            # rec.name += ' [' + rec.visit_from_date + ']'
-            # rec.name += ' - [' + rec.visit_to_date + ']'
+            rec.name = str(rec.doctor_id.name) + ' - [' + str(rec.visit_start_datetime) + ']'
+
+    @api.onchange('visit_start_datetime, doctor_id')
+    def _compute_visit_start_datetime(self):
+        #It bad works
+        sizes = self.search([('visit_start_datetime', '=', self.visit_start_datetime),
+                             ('doctor_id', '=', self.doctor_id.id)])
+        print('sizes---', len(sizes))
+        if len(sizes) != 0:
+            self.visit_start_datetime = ""
